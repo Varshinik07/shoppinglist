92ac5a0984301d20d65acbf279072da8cc76f225
Varshini <varshuk01@gmail.com>
2022-10-12 10:32:14 +0530

    shopping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { ShoppingComponent } from './shopping/shopping.component';
+import{RouterModule,Routes} from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",component:ShoppingComponent
+  },
+  
+  
+]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ShoppingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
